2a6fb7523329fcee22933c0d13d713690ba7ae59
yhojanssilva-hash <yhojanssilva@liceovvh.cl>
2026-04-15 09:59:12 -0400

    .

---
+++ 01_Introduccion/1_hola_mundo/static/js/script.js
@@ -0,0 +1,13 @@
+// Prueba de conexion con js
+console.log("Hola mundo, desde archivo externo!...");
+
+// 1) Hola mundo con variable y const.
+const saludo="Hola a todos, ";
+let destinatario = "estudiantes";
+// Impresion de saludo concatenando con (+)
+console.log(saludo + "para los " + destinatario);
+
+// 2) Saludar a una persona ingresada por teclado.
+let nombre = prompt("Ingrese su nombre: ");
+//Imprimir un nombre ingresado con un prompt
+console.log("Hola, " + nombre);
---
+++ 02_Tipos_de_datos/1.-Tipo_texto/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("Conexion con js correcta")
+/*
+Datp: Tipo texto(string)
+Concatenacion de texto con el signo+
+Podemos unir texto y variables
+*/
+const nombre = "Dany";
+const apellido = "Hernandez"
+//Unimos ambas constantes con un texto extra.
+console.log("Hola,mi nombre es: " + nombre + " " + apellido)
